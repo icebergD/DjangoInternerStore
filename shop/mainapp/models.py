8bc91cd4acf0676831305704d0c46164977bbf28
icebergD <snfmxmsc.s@gmail.com>
2021-11-29 18:07:16 +0300

    made some changes in model's archetecture

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
@@ -22,6 +23,10 @@ class Category(models.Model):
 		return self.name
 
 class Product(models.Model):
+
+	class Meta:
+		abstract = True
+
 	category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
 	title = models.CharField(max_length=255, verbose_name='Наименование')
 	slug = models.SlugField(unique=True)
@@ -32,11 +37,16 @@ class Product(models.Model):
 	def __str__(self):
 		return self.title
 
+
+
+
 class CartProduct(models.Model):
 
 	user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
-	cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE)
-	product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE, related_name='related_products')
+	cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
+	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+	object_id = models.PositiveIntegerField()
+	content_object = GenericForeignKey('content_type','object_id')
 	qty = models.PositiveIntegerField(default=1)
 	final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
@@ -63,10 +73,3 @@ class Customer(models.Model):
 	def __str__(self):
 		return 'Покупатель: {} {}'.format(self.user.first_name, self.user.last_name)
 
-class Specification(models.Model):
-	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-	object_id = models.PositiveIntegerField()
-	name = models.CharField(max_length=255, verbose_name='Имя товара для характеристик')
-
-	def __str__(self):
-		return 'Характеристики для товара {}'.format(selfself.name)
