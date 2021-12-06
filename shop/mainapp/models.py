59f1e8481e754127745ce5562c3432f49240a52d
icebergD <snfmxmsc.s@gmail.com>
2021-12-06 19:20:48 +0300

    finished model

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -6,6 +6,31 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 User = get_user_model()
 
 
+class LatestProductsManager:
+
+	@staticmethod
+	def get_products_for_main_page(*args, **kwargs):
+		with_respect_to = kwargs.get('with_respect_to')
+		products = []
+		ct_models = ContentType.objects.filter(model__in=args)
+		for ct_model in ct_models:
+			model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+			products.extend(model_products)
+		if with_respect_to:
+			ct_model = ContentType.objects.filter(model=with_respect_to)
+			if ct_model.exists():
+				if with_respect_to in args:
+					return sorted(
+						products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+					)
+		return products
+
+
+class LatestProducts:
+
+	objects = LatestProductsManager()
+
+
 class Category(models.Model):
 	name = models.CharField(max_length=255, verbose_name='Имя категории')
 	slug = models.SlugField(unique=True)
@@ -22,7 +47,7 @@ class Product(models.Model):
 	title = models.CharField(max_length=255, verbose_name='Наименование')
 	slug = models.SlugField(unique=True)
 	image = models.ImageField(verbose_name='Изображение')
-	description = models.TextField(verbose_name='Описание')
+	description = models.TextField(verbose_name='Описание', null=True)
 	price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
 
 	def __str__(self):
@@ -46,7 +71,7 @@ class Smartphone(Product):
 	resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
 	accum_volume = models.CharField(max_length=255, verbose_name='Объем батареи')
 	ram = models.CharField(max_length=255, verbose_name='Оперативная память')
-	sd = models.BinaryField(default=True)
+	sd = models.BooleanField(default=True)
 	sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальный объем встраеваемой памяти')
 	main_cam_mp = models.CharField(max_length=255, verbose_name='Главная камера')
 	frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
